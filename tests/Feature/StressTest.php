4f4a343a0e07e7b0bfc7a61da15b3d39267a92c7
Eduer Pallares <eduer.pallares@zalvadora.com>
2025-05-14 16:12:57 -0500

    linter

--- tests/Feature/StressTest.php
+++ tests/Feature/StressTest.php
@@ -1,18 +1,18 @@
 <?php
 
 use function Pest\Stressless\stress;
- 
+
 test('stress test with 1 concurrent request for 5 seconds', function () {
     $result = stress('http://localhost:8000')
         ->concurrently(1)
         ->for(5)->seconds()
         ->dump();
- 
+
     $requests = $result->requests;
- 
+
     expect($requests->failed->count)
         ->toBe(0);
- 
+
     expect($requests->duration->med)
         ->toBeLessThan(20);
 });
@@ -22,12 +22,12 @@ test('stress test with 5 concurrent requests for 10 seconds', function () {
         ->concurrently(5)
         ->for(10)->seconds()
         ->dump();
- 
+
     $requests = $result->requests;
- 
+
     expect($requests->failed->count)
         ->toBe(0);
- 
+
     expect($requests->duration->med)
         ->toBeLessThan(20);
 });
